3ed6902dcf9f314633cdacb0e707fd34b39bcf70
seanrogan <srogan88@gmail.com>
2022-03-27 18:38:29 -0400

    initial project template commit

---
+++ src/main/java/com/budget/app/BudgetTrackerAppApplication.java
@@ -0,0 +1,13 @@
+package com.budget.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BudgetTrackerAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BudgetTrackerAppApplication.class, args);
+	}
+
+}
